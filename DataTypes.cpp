45fd716c9b150c6ac0ac9772ffa88468d224aab5
EmmanuelU <emmanuelurias60@icloud.com>
2023-06-09 10:27:56 -0700

    More practice with variables

---
+++ DataTypes.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+    // integer (whole number)
+    int age = 18;
+    int year = 2023;
+
+    // double (number with decimal up to the 10 thousandth place)
+    double price = 10.99;
+    double gpa = 3.7;
+
+    // single characters
+    char grade = 'A';
+    char initial = 'E';
+
+    // Boolean (true or false)
+    bool isStudent = true;
+    bool onTime = false;
+
+    // String (objects that represents a sequence of text)
+    std::string name = "Emmanuel";
+    char lastName[] = "Urias";
+
+    std::cout << name << ' ' << lastName;
+
+    return 0;
+}
